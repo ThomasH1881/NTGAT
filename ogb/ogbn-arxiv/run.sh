286a960415f2d49a10672b8f681a4a20438c49d0
Wentao Hou <52404519+Thoment@users.noreply.github.com>
2022-07-02 14:41:12 +0800

    Add files via upload

---
+++ ogb/ogbn-arxiv/run.sh
@@ -0,0 +1,7 @@
+python gat.py --gpu=0 --lb=5 --k=0.99 >> curve.txt
+python gat.py --gpu=0 --lb=5 --k=0.95 >> curve.txt
+python gat.py --gpu=0 --lb=5 --k=0.9 >> curve.txt
+python gat.py --gpu=0 --lb=5 --k=0.8 >> curve.txt
+python gat.py --gpu=0 --lb=5 --k=0.75 >> curve.txt
+python gat.py --gpu=0 --lb=5 --k=0.5 >> curve.txt
+python gat.py --gpu=0 --lb=5 --k=0.25 >> curve.txt
